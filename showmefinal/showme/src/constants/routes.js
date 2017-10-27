3239d84de3c036c57d63df265f7b36fc2a13fd26
Nick Grismore <nicksk83@aol.com>
2017-10-27 14:49:10 -0400

    removed showmefinal

--- showmefinal/showme/src/constants/routes.js
+++
@@ -1,6 +0,0 @@
-export const LANDING = '/mainarea';
-export const SIGN_UP = '/signup';
-export const SIGN_IN = '/signin';
-export const PASSWORD_FORGET = '/pw-forget';
-export const HOME = '/home';
-export const ACCOUNT = '/account';
